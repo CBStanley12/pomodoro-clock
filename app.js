982dadb647eebb849d71a3845606518e7349f046
Christopher Stanley <cbstanley12@gmail.com>
2019-06-05 23:19:15 -0600

    feat: function to lead all event listeners

--- app.js
+++ app.js
@@ -11,6 +11,24 @@ var timerOn = false,
     minutes = getMins(duration),
     seconds = getSecs(duration, minutes);
 
+loadBtnEventListeners();
+
+function loadBtnEventListeners(){
+    document.querySelector('#break-increment').addEventListener('click', increment);
+    document.querySelector('#break-decrement').addEventListener('click', decrement);
+    document.querySelector('#session-increment').addEventListener('click', increment);
+    document.querySelector('#session-decrement').addEventListener('click', decrement);
+    document.querySelector('#start_stop').addEventListener('click', function(){
+        if(!timerOn){
+        timerOn = true;
+        startTimer();
+        } else {
+        stopTimer();
+        }
+    });
+    document.querySelector('#reset').addEventListener('click', resetTimer);
+}
+
 function increment(){}
 
 function decrement(){}
