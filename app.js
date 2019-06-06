507e5bdb7d0c7760122c5fcf0d81f915c9b8a335
Christopher Stanley <cbstanley12@gmail.com>
2019-06-05 23:15:08 -0600

    feat: declare variables and functions

---
+++ app.js
@@ -0,0 +1,32 @@
+const timerLabel = document.querySelector('#timer-label'),
+    timeLeft = document.querySelector('#time-left'),
+    breakLen = document.querySelector('#break-length'),
+    sessionLen = document.querySelector('#session-length');
+
+var timerOn = false,
+    onBreak = false,
+    sessionNum = parseInt(sessionLen.innerHTML),
+    breakNum = parseInt(breakLen.innerHTML),
+    duration = sessionNum * 60,
+    minutes,
+    seconds;
+
+function increment(){}
+
+function decrement(){}
+
+function countdown(num){}
+
+function startTimer(){}
+
+function stopTimer(){}
+
+function resetTimer(){}
+
+function setTime(num){}
+
+function displayTime(mins, secs){}
+
+function getSecs(dur, mins){}
+
+function getMins(dur){}
